f5ed51205c461ca4552e78f7c91b219e8825e575
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-01-06 17:33:19 -0800

    single photo showing the photo

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -14,7 +14,7 @@ const Photo = React.createClass({
                         <img src={post.display_src} alt={post.caption} className="grid-photo" />
                     </Link>
 
-                    <CSSTransitionGroup transitionName="like" transitionEterTimeout={500} transitionLeaveTimeout={500}>
+                    <CSSTransitionGroup transitionName="like" transitionEnterTimeout={500} transitionLeaveTimeout={500}>
                         <span key={post.likes} className="likes-heart">{post.likes}</span>
                     </CSSTransitionGroup>
                 </div>
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+//import comments
 
 const Single = React.createClass({
     render() {
+        //index of the post
+        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        console.log(i);
+        //get us the post
+        const post = this.props.posts[i];
+        console.log(post);
+
         return (
             <div className="single-photo">
-            I'm the single
+                 <Photo i={i} post={post} {...this.props} />
             </div>
         )
     }
