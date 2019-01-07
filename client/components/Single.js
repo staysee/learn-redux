03c26faae2a2ca7d95bbc72b3586bf62960bb9b1
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-01-06 17:48:40 -0800

    showing the single photo's comments and inputs for new comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import Photo from './Photo';
-//import comments
+import Comments from './Comments';
 
 const Single = React.createClass({
     render() {
+        const {postId} = this.props.params;
+
         //index of the post
-        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        const i = this.props.posts.findIndex((post) => post.code === postId);
         console.log(i);
         //get us the post
         const post = this.props.posts[i];
         console.log(post);
 
+        const postComments = this.props.comments[postId] || [];
+
         return (
             <div className="single-photo">
                  <Photo i={i} post={post} {...this.props} />
+                 <Comments postComments={postComments}/>
             </div>
         )
     }
