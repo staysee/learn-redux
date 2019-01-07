e6e828fe068e296efd2808e36b7e55bb69d9554b
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-01-06 18:42:17 -0800

    setup store to use redux devtools extension

--- client/store.js
+++ client/store.js
@@ -14,7 +14,12 @@ const defaultState = {
     comments
 };
 
-const store = createStore(rootReducer, defaultState);
+//create store enhancers so we can use Redux Dev Tools
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
